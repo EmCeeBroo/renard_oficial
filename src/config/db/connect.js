c3b0f9691479c000ce38e5aef927c010448fcda6
EmCeeBroo <alejopeace1@gmail.com>
2025-04-02 22:32:35 -0500

    Manitos he actualizado todo el proyecto de manera eficiente, ya con esto pueden clonarlo en sus equipos he ir analizando lo que vayan a realizar en el proyeto, ire adadtanado la base de datos que creo el estiado Semental y con esto Braan puede implementar las vistas del front. cualquier cambio comentenlo en el codigo y de paso hagan commits si es necesario

--- src/config/db/connect.js
+++ src/config/db/connect.js
@@ -1,11 +1,15 @@
-import dotnev from 'dotnev';
-import { createPool } from "mysql2/promise";
+// src/config/database.js
+require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-dotnev.config();
-export const connect = createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-    port:process.env.DB_PORT
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+module.exports = pool;
