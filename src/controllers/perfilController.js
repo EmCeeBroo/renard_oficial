ce9257148052f5df02a6483d705d4d1738b42838
RockyElSementalItaliano <cpenabuesaquillo@gmail.com>
2025-04-05 14:43:30 -0500

    Controlador perfil actualizacion
    se cambiaron hasta el momento el controlador para crear el perfil

--- src/controllers/perfilController.js
+++ src/controllers/perfilController.js
@@ -12,7 +12,7 @@ export const showPerfil = async (req, res) => {
 
 export const showPerfilId = async (req, res) => {
   try {
-    const [result] = await connect.query('SELECT * FROM perfil WHERE Perfil_id = ?', [req.params.id]);
+    const [result] = await connect.query('SELECT * FROM perfil WHERE id_perfil = ?', [req.params.id]);
     if (result.length === 0) return res.status(404).json({ error: "Perfil no encontrado" });
     res.status(200).json(result[0]);
   } catch (error) {
@@ -22,14 +22,14 @@ export const showPerfilId = async (req, res) => {
 
 export const addPerfil = async (req, res) => {
   try {
-    const { nombre, documento, correo, telefono, foto, direccion, tipo_documento,usuario_id } = req.body;
-    if (!nombre || !documento || !correo || !telefono) {
+    const { nombre, apellido, telefono, direccion, correo, foto, usuario, tipo_documento } = req.body;
+    if (!nombre ||!apellido || !telefono || !direccion || !correo || !tipo_documento) {
       return res.status(400).json({ error: "Los campos son obligatorios" });
     }
-    let sqlQuery = "INSERT INTO perfil (nombre_perfil,documento_perfil,correo_perfil,telefono_perfil,foto_perfil,direccion_perfil,tipo_documento_fk,usuario_fk) VALUES (?,?,?,?,?,?,?,?)";
+    let sqlQuery = "INSERT INTO perfil (nombre,apellido,telefono,direccion,correo,foto,usuario_fk,tipo_documento_fk) VALUES (?,?,?,?,?,?,?,?)";
     const [result] = await connect.query(sqlQuery, [nombre, documento, correo, telefono, foto, direccion, tipo_documento,usuario_id]);
     res.status(201).json({
-      data: [{ id: result.insertId, nombre, documento, correo, telefono, foto, direccion, tipo_documento,usuario_id }],
+      data: [{ id: result.insertId, nombre, apellido, telefono, direccion, correo, foto, usuario, tipo_documento }],
       status: 201
     });
   } catch (error) {
@@ -43,7 +43,7 @@ export const updatePerfil = async (req, res) => {
     if (!nombre || !documento || !correo || !telefono) {
       return res.status(400).json({ error: "Los campos son obligatorios" });
     }
-    let sqlQuery = "UPDATE perfil SET nombre_perfil=?,documento_perfil=?,correo_perfil=?,telefono_perfil=?,foto_perfil=?,direccion_perfil=?,tipo_documento_fk=?,Updated_at=? WHERE Perfil_id = ?";
+    let sqlQuery = "UPDATE perfil SET nombre_perfil=?,documento_perfil=?,correo_perfil=?,telefono_perfil=?,foto_perfil=?,direccion_perfil=?,tipo_documento_fk=?,Updated_at=? WHERE id_perfil = ?";
     const updated_at = new Date().toLocaleString("en-CA", { timeZone: "America/Bogota" }).replace(",", "").replace("/", "-").replace("/", "-");
     const [result] = await connect.query(sqlQuery, [nombre, documento, correo, telefono, foto, direccion, tipo_documento, updated_at, req.params.id]);
     if (result.affectedRows === 0) return res.status(404).json({ error: "Perfil no encontrado" });
@@ -59,7 +59,7 @@ export const updatePerfil = async (req, res) => {
 
 export const deletePerfil = async (req, res) => {
   try {
-    let sqlQuery = "DELETE FROM perfil WHERE perfil_id = ?";
+    let sqlQuery = "DELETE FROM perfil WHERE id_perfil = ?";
     const [result] = await connect.query(sqlQuery, [req.params.id]);
     if (result.affectedRows === 0) return res.status(404).json({ error: "Perfil no encontrado" });
     res.status(200).json({
